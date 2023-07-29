f3ea10f5613a3f6bde17bf5359206ed117a6c13a
YeonHoLee-dev <yeonholee.dev@gmail.com>
2023-07-29 20:52:01 +0900

    feat(widget): Add textview

--- Part1/Widget/app/src/main/java/com/yeonholee/widget/MainActivity.kt
+++ Part1/Widget/app/src/main/java/com/yeonholee/widget/MainActivity.kt
@@ -1,46 +1,22 @@
 package com.yeonholee.widget
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.yeonholee.widget.ui.theme.WidgetTheme
+import com.yeonholee.widget.databinding.TextViewBinding
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: TextViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            WidgetTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WidgetTheme {
-        Greeting("Android")
+        binding = TextViewBinding.inflate(layoutInflater) // 뷰 바인딩 인스턴스 생성
+        val view = binding.root // 뷰 바인딩의 루트 뷰에 접근
+        setContentView(view) // 액티비티에 뷰 바인딩 루트 뷰 설정
+        
+        binding.text1.text = "안녕하세요"
+        binding.text1.setBackgroundColor(Color.argb(100,100,100,100))
+        binding.text1.setTextColor(Color.BLUE)
+        binding.text1.append(" 감사합니다.")
     }
 }
